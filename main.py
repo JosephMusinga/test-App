50a166699294e62b44a0428bf12b349e6d384259
Jos <98326351+JosephMusinga@users.noreply.github.com>
2024-05-01 07:20:25 +0200

    write to .ass via interface

--- main.py
+++ main.py
@@ -8,14 +8,7 @@ import ffmpeg
 import whisper
 import tkinter
 
-def pick_color(button, color_option):
-    pick_color = AskColor()
-    if pick_color:
-        color = pick_color.get()  # Get the hexadecimal color code
-        print(color)
-        button.configure(**{color_option: color})
-        return color
-    return None
+
 
 
 def customize_transcript():
@@ -32,7 +25,7 @@ def customize_transcript():
     y = (hs/2) - (h/2)
     toplevel2.geometry('%dx%d+%d+%d' % (w, h, x, y))
     
-    # Font Style and Size
+    # Font Style and Size comboboxes
     def update_font_style_var(*args):
         selected_font_style = font_style_var.get()
         print("Selected font style:", selected_font_style)
@@ -84,65 +77,88 @@ def customize_transcript():
     not_italic_radio = customtkinter.CTkRadioButton(master=toplevel2, variable=italic_var, text="Not Italic", value=0, command=update_italic_var)
     not_italic_radio.pack(padx=5, pady=5)
     
-    # Color Input Fields
-    primary_color_button = customtkinter.CTkButton(master=toplevel2, text="PrimaryColor", command=lambda: pick_color(primary_color_button, 'fg_color'))
+    # Color Buttons
+    def pick_color(button, color_option):
+        pick_color = AskColor()
+        if pick_color:
+            color = pick_color.get()
+            button.configure(**{color_option: color})
+            return color
+        return None
+    
+    def update_primary_color():
+        global primary_color
+        primary_color = pick_color(primary_color_button, 'fg_color')
+        print(primary_color)
+
+    primary_color_button = customtkinter.CTkButton(master=toplevel2, text="PrimaryColor", command=update_primary_color)
     primary_color_button.pack(padx=5, pady=5)
+    
+    def update_secondary_color():
+        global secondary_color
+        secondary_color = pick_color(secondary_color_button, 'fg_color')
+        print(secondary_color)
 
-    secondary_color_button = customtkinter.CTkButton(master=toplevel2, text="Secondary Color", command=lambda: pick_color(secondary_color_button, 'fg_color'))
+    secondary_color_button = customtkinter.CTkButton(master=toplevel2, text="Secondary Color", command=update_secondary_color)
     secondary_color_button.pack(padx=5, pady=5)
+    
+    def update_outline_color():
+        global outline_color
+        outline_color = pick_color(outline_color_button, 'fg_color')
+        print(outline_color)
 
-    outline_color_button = customtkinter.CTkButton(master=toplevel2, text="Outline Color", command=lambda: pick_color(outline_color_button, 'fg_color'))
+    outline_color_button = customtkinter.CTkButton(master=toplevel2, text="Outline Color", command=update_outline_color)
     outline_color_button.pack(padx=5, pady=5)
     
-    background_color_button = customtkinter.CTkButton(master=toplevel2, text="Background Color", command=lambda: pick_color(background_color_button, 'fg_color'))
+    def update_background_color():
+        global background_color
+        background_color = pick_color(background_color_button, 'fg_color')
+        print(background_color)
+    
+    background_color_button = customtkinter.CTkButton(master=toplevel2, text="Background Color", command=update_background_color)
     background_color_button.pack(padx=5, pady=5)
     
-    #primary_color, secondary_color, outline_color = "", "", ""
     
-    # def modify_style_section(text, font_style, font_size, bold_var, italic_var, primary_color, secondary_color, outline_color):
-    #     """
-    #     Modifies the [V4+ Styles] section in the text with the user-provided style and size.
-    #     """
-    #     lines = text.splitlines()
-    #     for i, line in enumerate(lines):
-    #         if line.startswith("Style: Default,"):
-    #             # Split the style definition
-    #             style_parts = line.split(",")
-    #             # Update style properties
-    #             style_parts[1] = font_style  # Font style
-    #             style_parts[2] = str(font_size)  # Font size
-    #             if bold_var.get():
-    #                 style_parts[7] = str(int(input("Enter desired bold value (0 or 1): ")))  # Bold
-    #             if italic_var.get():
-    #                 style_parts[8] = str(int(input("Enter desired italic value (0 or 1): ")))  # Italic
-    #             style_parts[4] = primary_color or style_parts[4]  # Primary color (use existing if not provided)
-    #             style_parts[5] = secondary_color or style_parts[5]  # Secondary color (use existing if not provided)
-    #             style_parts[6] = outline_color or style_parts[6]  # Outline color (use existing if not provided)
-    #             # style_parts[9] =  or style_parts[9]  # Back color (use existing if not provided)
-    #             # Join the modified parts back with comma separators
-    #             lines[i] = ",".join(style_parts)
-    #             break  # Stop after modifying the first occurrence
-    #     return "\n".join(lines)
+    def modifications_section(text, font_style, font_size, p_color, s_color, out_color, back_color, bold, italic):
+        """
+        Modifies the [V4+ Styles] section in the text with the user-provided style and size.
+        """
+        lines = text.splitlines()
+        for i, line in enumerate(lines):
+            if line.startswith("Style: Default,"):
+                style_parts = line.split(",")
+                style_parts[1] = font_style 
+                style_parts[2] = str(font_size)
+                style_parts[3] = p_color or style_parts[3]
+                style_parts[4] = s_color or style_parts[4]
+                style_parts[5] = out_color or style_parts[5] 
+                style_parts[6] = back_color or style_parts[6]
+                style_parts[7] = bold or style_parts[7]
+                style_parts[8] = italic or style_parts[8]
+                # Join the modified parts back with comma separators
+                lines[i] = ",".join(style_parts)
+                break  # Stop after modifying the first occurrence
+        return "\n".join(lines)
 
-    # def apply_changes():
-    #     with open("sub-martin.en.ass", "r") as f:
-    #         text = f.read()
+    def apply_changes():
+        with open("sub-martin.en.ass", "r") as f:
+            text = f.read()
 
-    #     font_style = font_style_var.get()
-    #     font_size = font_size_var.get()
-    #     bold = bold_var.get()
-    #     italic = italic_var.get()
+        font_style = font_style_var.get()
+        font_size = font_size_var.get()
+        bold = str(bold_var.get())
+        italic = str(italic_var.get())
 
-    #     modified_text = modify_style_section(text, font_style, font_size, bold, italic, primary_color, secondary_color, outline_color)
+        modified_text = modifications_section(text, font_style, font_size, primary_color, secondary_color, outline_color, background_color, bold, italic)
 
-    #     with open("sub-martin.en.ass", "w") as f:
-    #         f.write(modified_text)
+        with open("new.en.ass", "w") as f:
+            f.write(modified_text)
 
-    #     print("Successfully modified the font style and size in your .ass file!")
+        print("Successfully modified the font style and size in your .ass file!")
 
 
 
-    apply_modifications_button = customtkinter.CTkButton(master=toplevel2, text="Apply Changes") #, command=apply_changes
+    apply_modifications_button = customtkinter.CTkButton(master=toplevel2, text="Apply Changes", command=apply_changes) #
     apply_modifications_button.pack(padx=5, pady=5)
     
     
@@ -208,17 +224,20 @@ class App(customtkinter.CTk):
         super().__init__(*args, **kwargs)
         self.geometry("450x600")
 
-        self.button_1 = customtkinter.CTkButton(self, text="Open File", command=self.open_file)
-        self.button_1.pack(side="top", padx=20, pady=20)
+        self.open_file_button = customtkinter.CTkButton(self, text="Open File", command=self.open_file)
+        self.open_file_button.pack(side="top", padx=20, pady=20)
 
         self.label = customtkinter.CTkLabel(self, text="No item selected", text_color="white")
         self.label.pack(padx=20, pady=20)
 
-        self.button_2 = customtkinter.CTkButton(self, text="Generate Transcript", command=self.transcribe)
-        self.button_2.pack(side="top", padx=20, pady=20)
+        self.generate_transcript_button = customtkinter.CTkButton(self, text="Generate Transcript", command=self.transcribe)
+        self.generate_transcript_button.pack(side="top", padx=20, pady=20)
         
-        self.button_3 = customtkinter.CTkButton(self, text="Customize Captions", command=customize_transcript)
-        self.button_3.pack(side="top", padx=20, pady=20)
+        self.customize_captions_button = customtkinter.CTkButton(self, text="Customize Captions", command=customize_transcript)
+        self.customize_captions_button.pack(side="top", padx=20, pady=20)
+        
+        self.add_captions_to_video_button = customtkinter.CTkButton(self, text="Add captions to video")
+        self.add_captions_to_video_button.pack(side="top", padx=20, pady=20)
 
         self.toplevel_window = None
         self.copied_video = None  # Track the copied video path
--- sandbox copy.py
+++ sandbox copy.py
@@ -1,20 +1,4 @@
-from customtkinter import CTkColorPicker
-import customtkinter
+line = "Style: Default,Arial,16,&Hffffff,&Hffffff,&H0,&H0,0,0,0,0,100,100,0,0,1,1,0,2,10,10,10,1"
 
-primary_color_var = tk.StringVar(toplevel2)  # Global variable to store the chosen color
-
-def pick_primary_color():
-    """
-    Opens a color picker and sets the chosen color in the primary_color_var variable.
-    """
-    color = CTkColorPicker(master=toplevel2).show()  # Open the color picker
-    if color:
-        primary_color_var.set(color)  # Set the color variable if a color is chosen
-
-primary_color_label = customtkinter.CTkLabel(master=toplevel2, text="Primary Color:")
-primary_color_label.pack(padx=5, pady=5)
-
-primary_color_button = customtkinter.CTkButton(
-    master=toplevel2, text="Pick", command=pick_primary_color
-)
-primary_color_button.pack(padx=5, pady=5)
+if line.startswith("Style: Default,"):
+    style_parts = line.split(",")
